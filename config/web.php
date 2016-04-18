c9556ba60f90a3af730f239c4eb70d327b935f6a
mmerzlyakov <alpmsm@ya.ru>
2016-04-18 10:01:04 +0600

    15/04/2016

--- config/web.php
+++ config/web.php
@@ -7,6 +7,14 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
+        'view' => [
+            'renderers' => [
+                'tpl' => [
+                    'class' => 'yii\smarty\ViewRenderer',
+                    //'cachePath' => '@runtime/Smarty/cache',
+                ],
+            ],
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'dOUdMUqVGVrjNidVJzRoPir9EOkh68kL',
